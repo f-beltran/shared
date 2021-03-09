3a4cb60fd3545a7ef044c470e1f8c808b08198f9
Franz Beltran <fbeltran@ucb.edu.bo>
2021-03-09 13:04:27 -0400

    Install Angular Material

--- libs/shared/ui-material/src/lib/shared-ui-material.module.ts
+++ libs/shared/ui-material/src/lib/shared-ui-material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooComponent } from './foo/foo.component';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MatSliderModule],
   declarations: [FooComponent],
-  exports: [FooComponent],
+  exports: [FooComponent, MatSliderModule],
 })
 export class SharedUiMaterialModule {}
